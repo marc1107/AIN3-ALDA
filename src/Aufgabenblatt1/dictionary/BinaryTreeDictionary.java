5abfa722bf2ab3f6dc3b87c2de85fff1898cabe1
marc1107 <m.bohner99@gmail.com>
2022-11-23 11:11:41 +0100

    BinaryTreeDictionary angefangen

--- src/Aufgabenblatt1/dictionary/BinaryTreeDictionary.java
+++ src/Aufgabenblatt1/dictionary/BinaryTreeDictionary.java
@@ -7,15 +7,15 @@ import java.util.Iterator;
 /**
  * Implementation of the Dictionary interface as AVL tree.
  * <p>
- * The entries are ordered using their natural ordering on the keys, 
- * or by a Comparator provided at set creation time, depending on which constructor is used. 
+ * The entries are ordered using their natural ordering on the keys,
+ * or by a Comparator provided at set creation time, depending on which constructor is used.
  * <p>
  * An iterator for this Aufgabenblatt1.dictionary is implemented by using the parent node reference.
- * 
+ *
  * @param <K> Key.
  * @param <V> Value.
  */
-public class BinaryTreeDictionary<K, V> implements Dictionary<K, V> {
+public class BinaryTreeDictionary<K extends Comparable<? super K>, V> implements Dictionary<K, V> {
 
     static private class Node<K, V> {
         K key;
@@ -35,29 +35,104 @@ public class BinaryTreeDictionary<K, V> implements Dictionary<K, V> {
         }
     }
 
+    private static class MinEntry<K, V> {
+        private K key;
+        private V value;
+    }
+
     private Node<K, V> root = null;
     private int size = 0;
+    private V oldValue;
 
     // ...
 
     @Override
     public V insert(K key, V value) {
-        return null;
+        root = insertR(key, value, root);
+        if (root != null) {
+            root.parent = null;
+        }
+        return oldValue;
+    }
+
+    private Node<K, V> insertR(K key, V value, Node<K, V> p) {
+        if (p == null) {
+            p = new Node(key, value);
+            oldValue = null;
+        } else if (key.compareTo(p.key) < 0) {
+            p.left = insertR(key, value, p.left);
+            if (p.left != null) {
+                p.left.parent = p;
+            }
+        } else if (key.compareTo(p.key) > 0) {
+            p.right = insertR(key, value, p.right);
+            if (p.right != null) {
+                p.right.parent = p;
+            }
+        } else {
+            oldValue = p.value;
+            p.value = value;
+        }
+        return p;
     }
 
     @Override
     public V search(K key) {
-        return null;
+        return searchR(key, root);
+    }
+
+    private V searchR(K key, Node<K, V> p) {
+        if (p == null)
+            return null;
+        else if (key.compareTo(p.key) < 0)
+            return searchR(key, p.left);
+        else if (key.compareTo(p.key) > 0)
+            return searchR(key, p.right);
+        else
+            return p.value;
     }
 
     @Override
     public V remove(K key) {
-        return null;
+        root = removeR(key, root);
+        return oldValue;
+    }
+
+    private Node<K, V> removeR(K key, Node<K, V> p) {
+        if (p == null) {
+            oldValue = null;
+        } else if (key.compareTo(p.key) < 0) {
+            p.left = removeR(key, p.left);
+        } else if (key.compareTo(p.key) > 0) {
+            p.right = removeR(key, p.right);
+        } else if (p.left == null || p.right == null) {
+            oldValue = p.value;
+            p = (p.left != null) ? p.left : p.right;
+        } else {
+            MinEntry<K, V> min = new MinEntry<K, V>();
+            p.right = getRemMinR(p.right, min);
+            oldValue = p.value;
+            p.key = min.key;
+            p.value = min.value;
+        }
+        return p;
+    }
+
+    private Node<K, V> getRemMinR(Node<K, V> p, MinEntry<K, V> min) {
+        assert p != null;
+        if (p.left == null) {
+            min.key = p.key;
+            min.value = p.value;
+            p = p.right;
+        } else {
+            p.left = getRemMinR(p.left, min);
+        }
+        return p;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
@@ -65,10 +140,10 @@ public class BinaryTreeDictionary<K, V> implements Dictionary<K, V> {
         return null;
     }
 
-	/**
-	 * Pretty prints the tree
-	 */
-	public void prettyPrint() {
+    /**
+     * Pretty prints the tree
+     */
+    public void prettyPrint() {
         printR(0, root);
     }
 
